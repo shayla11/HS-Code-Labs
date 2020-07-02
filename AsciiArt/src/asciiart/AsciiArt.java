3db6ec1d29be461887c72f301570a65007eb592f
Shayla Sexton <sesexton@aggies.ncat.edu>
2020-07-01 21:53:07 -0500

    add asciiart project

---
+++ AsciiArt/src/asciiart/AsciiArt.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asciiart;
+
+/**
+ *
+ * @author shayla
+ */
+public class AsciiArt {
+
+    public static void main(String[] args) {
+        System.out.println("Shayla Sexton \n\n" );
+        System.out.println("What type of ANIMAL YOU WILL DRAW" );
+        System.out.println("\n\n\n\n" );
+
+        System.out.println("                      ,.                                   ");
+        System.out.println("                     (_|,.                                 ");
+        System.out.println("                    ,' /, )_______   _                     ");
+        System.out.println("                 __j o ``-'       `.'-)'                   ");
+        System.out.println("                (\")                 \\'                   ");
+        System.out.println("                 `-j                   |                   ");
+        System.out.println("                   `-._(              /                    ");
+        System.out.println("                      |_|  |--^.     /                     ");
+        System.out.println("                     /_] ' |_|  /_)_/                      ");
+        System.out.println("                          /_]'   /_]'                      ");
+        System.out.println("                                                           ");
+        System.out.println("                                                           ");
+
+    }
+    
+}
